075f31ea59462bad7e028c5ca36e24744ef61dde
Isabel Broome-Nicholson <isabel.broomenicholson@gmail.com>
2013-06-05 21:23:20 +1200

    got restful things working, worked out how post and responses work

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var mongoose = require ("mongoose"); 
+var fs = require("fs");
+
 
 // find appropriate db to connect to, default to localhost
 var uristring = 
@@ -10,6 +12,13 @@ var uristring =
 // the appropriate port, or default to 5000  
 var port = process.env.PORT || 5000;
 
+
+// set up the server
+var app = express();
+app.use(express.logger());
+app.use(express.bodyParser());  // allows the app to read JSON from body
+
+
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
 mongoose.connect(uristring, function (err, res) {
@@ -19,6 +28,10 @@ mongoose.connect(uristring, function (err, res) {
     console.log ('Succeeded connected to: ' + uristring);
   }
 });
+
+
+// Define the User object
+//----------------------------------------------------------------------------
 
 // This is the schema.  Note the types, validation and trim
 // statements.  They enforce useful constraints on the data.
@@ -34,24 +47,72 @@ var userSchema = new mongoose.Schema({
 // nonexistent) the 'PowerUsers' collection in the MongoDB database
 var PUser = mongoose.model('PowerUsers', userSchema);
 
+//----------------------------------------------------------------------------
 
-// The webpages
-//-----------------------
-var app = express();
-app.use(express.logger());
 
-// GET '/'
-app.get('/', function(request, response){
-	response.send('options are: ' + uristring + '/AddPerson or ' + uristring + '/GetPeople' );
+
+
+
+
+// The API end points
+//--------------------------------------------------------------------------------------------------------------
+var errorFunction = function(err){if (err) console.log('Error on save!')};
+
+
+//
+// eg.  Content-Type: application/json
+//      {"name":{"first":"isabel",  "last":"Broome-Nicholson"},"age":25}
+//
+// POST '/user'
+// creates a user, and returns them
+app.post('/user', function(request, response){
+
+	// read the values of the post body
+	var firstname = request.body.name.first;
+	var lastname = request.body.name.last;
+	var age = request.body.age;
+
+	// create a new user 
+	var newUser = new PUser ({
+		name: { first: firstname, last: lastname },
+		age: age
+	});
+	
+	// save them
+	newUser.save(errorFunction);
+
+	// send the response 
+	response.send(newUser);  // returns it as application/json
 });
 
 
-// GET '/GetPeople'
-app.get('/GetPeople', function(request, response) {
+// GET '/user/{id}'
+// returns the user with the given id
+app.get('/user/:id', function(request, response){
+
+	var id = request.params.id;
+
+	// define the query
+	var query = PUser.findById(id);
+
+	// execute the query
+	query.exec(function(err, result) {
+		if (!err){
+			response.send(result); 
+		}
+		else {
+			response.send(500, 'An error happened with the query');  // returns the error code
+		}
+	});
+});
+
+
+// GET '/user'
+// returns all users
+app.get('/user', function(request, response) {
 
 	// make a query to find some users
-	var query = PUser.find({'name.last': 'Doe'}); 
-	query.where('age').gt(24);
+	var query = PUser.find({'age': 25}); 
 	
 	// return all the results so far
 	query.exec(function(err, result) {
@@ -59,27 +120,10 @@ app.get('/GetPeople', function(request, response) {
 			response.send(JSON.stringify(result, undefined, 2));
 		}
 		else {
-			response.send('An error happened with the query');
+			response.send(500, 'An error happened with the query');
 		}
 	});
 });
-
-// GET '/AddPerson'   <--- could be post
-app.get('/AddPerson', function(request, response){
-	// create a new user
-	var johndoe = new PUser ({
-		name: { first: 'John', last: '  Doe   ' },
-		age: 25
-	});
-	
-	// save them
-	johndoe.save(function (err) {if (err) console.log('Error on save!')});
-	
-	// return response
-	response.send('Successfully added a new person!' );
-});
-
-
 
 
 app.listen(port, function() {
